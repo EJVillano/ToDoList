767ce16506bac06ac51964c16032fc183e7871e1
EJVillano <18ejvillano@gmail.com>
2025-01-26 21:58:03 +0800

    add task schema

---
+++ Models/Tasks.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const taskScema = new mongoose.Schema({
+
+    task: {
+        type: String,
+        required: [true, 'Task is Required']
+    },
+    Description: {
+        type: String,
+        required: false
+    },
+    status: {
+        type: String,
+        enum: ['not started', 'In ProgressEvent', 'Done'],
+        default: 'not started'
+    },
+    dueDate: {
+        type: Date,
+        required: true
+    },
+    completedAt: {
+        type: Date,
+        default: null
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+})
