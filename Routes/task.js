f63efa596a131f5a033962c6d18def0eb389487c
EJVillano <18ejvillano@gmail.com>
2025-01-28 00:03:07 +0800

    add create task route

--- Routes/task.js
+++ Routes/task.js
@@ -1,2 +1,12 @@
 //[Dependencies and Modules]
 const express = require("express")
+
+// [Routing Component]
+const router = express.Router();
+const taskController = require("../controllers/task.js")
+
+// [Routes]
+router.post("/", taskController.createTask)
+
+// [Export Route System]
+module.exports = router;
